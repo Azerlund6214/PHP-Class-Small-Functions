115361e7c336439fd52c0fe1b392aaa6b58e324a
Azerlund6214 <67274297+Azerlund6214@users.noreply.github.com>
2020-07-27 15:55:37 +0300

    Первичный залив

---
+++ Demo.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+	//require_once "SF_CLASS.php";
+	//SF::PRINTER(Array("123","12"));
+	//SF::Print_Class_Func_and_Vars($UN_YOUTUBE);
+	//SF::Memory_Usage_EchoGet("M","Echo");
+	//SF::Echo_This_File_Path();
+	//echo SF::Get_This_Server_Domain(true,true,"213");
+	//SF::PRINTER( SF::Get_Server_Headers() );
+	//SF::PRINTER( SF::Get_HTTP_Response() );
+	
+
+
+
+
+
+?>
